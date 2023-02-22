35799015847499686cbdb7d7ef7ad3f125fa3f4b
mperezpizarro <marioperezpizarro@gmail.com>
2023-02-22 08:58:07 +0100

    Improved search function

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -9,7 +9,7 @@
         <PageTitle>BUSCAR FICHAS</PageTitle>
         <section class="mt-4">
             <p>Para buscar una ficha, introduce el nombre del contenido que quieras buscar.</p>
-            <p>(Nota: De momento sólo funciona si buscas el título traducido)</p>
+            <p>(Nota: Puedes introducir tanto el título original como traducido.)</p>
         </section>
     </div>
 </template>
--- server/api/searchFicha.js
+++ server/api/searchFicha.js
@@ -4,7 +4,7 @@ export default defineEventHandler((ev) => {
     const { search } = getQuery(ev)
 
     const resultado = fichas.filter((ficha) => {
-        return ficha.info_tecnica.titulo.toLowerCase().includes(search)
+        return ficha.info_tecnica.titulo.toLowerCase().includes(search) || ficha.info_tecnica.titulo_vo.toLowerCase().includes(search)
     })
 
     return resultado
